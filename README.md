0061d2b64c27cb8cd3f27ebe276df6b17cada5e8
Jac <kuruenai@gmail.com>
2026-09-18 10:15:09 +1000

    Rewrote pseudocode into comments for more clarity

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 This is the first project assignemnt from The Odin Project's Javascript Basics Course.
 
-This is my first project involving javascript, as well as using pseudocode to help solve problems
-I'm not using arrays because TOP is covering that later in the curriculum. 
+This is my first project involving javascript, as well as using pseudocode to help solve problems.
+
+I'm not using arrays or loops because TOP is covering that later in the curriculum. 
+Hence the if/else statements and why the round functions are repeatedly declared.
--- javascript.js
+++ javascript.js
@@ -1,123 +1,16 @@
-/*
--- Write a function that randomly returns 'rock', 'paper', or 'scissors' --
-
-Assign a variable 'choice' to a random number:
-    Generate a random number that is greater than or equal to 0, and less than 1
-    The random number is multiplied by 3, and rounded down to the nearest whole (0, 1, or 2)
-
-If the choice variable is between 0, print 'rock'
-If the choice variable is between 1, print 'paper'
-If the choice variable is between 2, print 'scissors'
-*/
-
-/*
-function getComputerChoice() {
-    let choiceNum = Math.floor(Math.random() * 3);
-    //console.log(choiceNum); // Checking the integer associated with the computerChoice
-    if (choiceNum === 0) {
-        console.log('Computer: Rock');
-        return 'rock';
-    } else if (choiceNum === 1) {
-        console.log('Computer: Paper');
-        return 'paper';
-    } else if (choiceNum === 2) {
-        console.log('Computer: Scissors');
-        return 'scissors';
-    }
-}
-*/
-
-/*
--- Write a function that takes the user choice and returns it --
-
-Ask the user to write their choice
-Provide a space where the user can write their choice
-Return that choice
-*/
-
-/*
-function getHumanChoice() {
-    let choicePrompt = prompt("Paper, scissors, rock!");
-    console.log('Human: ' + choicePrompt);
-    return choicePrompt
-}
-*/
-
-/*
--- Write a function that takes the human and computer player choices as arguments, 
-    plays a single round, incremenets the round winner's score, 
-    and logs a winner announcement --
-
-Create a function called playRound - plays a round of RPS
-    Define two parameters for this function: humanChoice, computerChoice
-    Make the humanChoice parameter case insensitive for input variation
-        Convert all inputs into lowercase for the parameter
-
-    Define rules of the game
-        Rock -> Scissors -> Paper -> Rock
-
-        If human choice is equal to computer choice it's a tie
-
-        If human choice is rock AND computer choice is scissors, human wins
-        If human choice is paper AND computer chouce is rock, human wins
-        If human chouce is scissors AND computer chouce is paper, human wins
-
-        All other instances, the computer will win (e.g. human=scissors, computer=rock)
-
-playRound function logs a string value into the console representing the round winner
-Increments the humanScore or computerScore variable based on the round winner
-*/
-
-/*
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice.toLowerCase() === computerChoice) {
-        console.log("It's a tie!");
-    } else if (
-       ((humanChoice.toLowerCase() == 'rock') && (computerChoice == 'scissors'))
-    || ((humanChoice.toLowerCase() == 'paper') && (computerChoice == 'rock'))
-    || ((humanChoice.toLowerCase() == 'scissors') && (computerChoice == 'paper'))) {
-        console.log("You win!");
-    } else {
-        console.log("You lose!");
-    }
-}
-
-const humanSelection = getHumanChoice(); // Returning inputs
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection); // Calling the function with the new inputs.
-*/
-
-/*
--- Write a function that plays five rounds, keeps track of score, and then declares a winner --
-
-A function playGame will call the playRound function five times
-    The playRound function will be declared inside of the playGame function
-    The humanScore and computerScore variables will also be inside playGame
-    
-    Each playRound call will +1 to either variable, depending on who wins
-        If human wins, logs +1 to human and console logs "You win!" -- shows score
-        If computer wins, logs +1 to computer and console logs "You lose!" -- shows score
-        If it's a tie, neither gets +1 and console logs "It's a tie!" -- shows score
-
-    playRound will be called 5 times
-    After the fifth call, a winner will be declared
-        The score variables will be compared, a message will  be logged accordingly
-*/
-
 function playGame() {
 
-    let humanScore = 0;
+    let humanScore = 0;     // Defining variables for keeping score
     let computerScore = 0;
 
-    function getComputerChoice() {
-        let choiceNum = Math.floor(Math.random() * 3);
+    function getComputerChoice() {  // Function to calculate a random computer choice
+        let choiceNum = Math.floor(Math.random() * 3); // Randomly generates either 0, 1, or 2
         //console.log(choiceNum); // Checking the integer associated with the computerChoice
         if (choiceNum === 0) {
-            console.log('Computer: Rock');
-            return 'rock';
+            console.log('Computer: Rock');  // What the player will see
+            return 'rock';                  // What is assigned internally
         } else if (choiceNum === 1) {
-            console.log('Computer: Paper');
+            console.log('Computer: Paper'); // ...and so on
             return 'paper';
         } else if (choiceNum === 2) {
             console.log('Computer: Scissors');
@@ -125,36 +18,36 @@ function playGame() {
         }
     }
 
-    function getHumanChoice() {
-        const input = prompt("Paper, scissors, rock!");
-        console.log('Human: ' + input);
-        return input.toLowerCase();
+    function getHumanChoice() { // Outputs the choice of the player/human
+        const input = prompt("Paper, scissors, rock!"); // Prompt box appears asking for input
+        console.log('Human: ' + input);                 // Shows player what they wrote
+        return input.toLowerCase();     // Returns a case insensitive string for comparisons
     }
 
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === computerChoice) {
+    function playRound(humanChoice, computerChoice) { // Compares player and computer choice
+        if (humanChoice === computerChoice) { // If they have the same choice
             console.log("It's a tie!");
-        } else if (
-           ((humanChoice == 'rock') && (computerChoice == 'scissors')) || 
-           ((humanChoice == 'paper') && (computerChoice == 'rock')) || 
-           ((humanChoice == 'scissors') && (computerChoice == 'paper'))
+        } else if ( // Conditions for the human to win                     // H:     C:
+           ((humanChoice == 'rock') && (computerChoice == 'scissors')) ||  // Rock > Scissors
+           ((humanChoice == 'paper') && (computerChoice == 'rock')) ||     // Paper > Rock
+           ((humanChoice == 'scissors') && (computerChoice == 'paper'))    // Scissors > Paper
         ) {
-            console.log("You win!");
-            humanScore++
+            console.log("You win!"); 
+            humanScore++        // Increases humans score by one
             return humanScore;
-        } else {
+        } else {                // Every other condition should result in the computer winning
             console.log("You lose!");
-            computerScore++;
+            computerScore++;    // Increases computers score by one
             return computerScore;
         }
     }
 
-    function declareWinner() {
-        if (humanScore > computerScore) {
+    function declareWinner() {  // Calculates who won the game
+        if (humanScore > computerScore) {   // If human has more points
             console.log('Human wins the game!');
-        } else if (computerScore > humanScore) {
+        } else if (computerScore > humanScore) { // If computer has more points
             console.log('Computer wins the game!');
-        } else {
+        } else { // Everything else is a tie (i.e. if the computer and human have equal points)
             console.log('The game is a tie!');
         }
     }
@@ -184,7 +77,7 @@ function playGame() {
     playRound(humanSelection, computerSelection); // Playing a final round
     console.log('Human: ' + humanScore + ' | Computer: ' + computerScore); // Show score update
 
-    declareWinner();
+    declareWinner(); // Call the function that declares a winner
 }
 
-playGame();
+playGame(); // Calls the function to play the game (five rounds)
